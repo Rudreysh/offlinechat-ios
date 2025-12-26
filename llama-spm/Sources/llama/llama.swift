ac2ac57d62802e108fba6cf5fede9d8802bc893e
rukesh <rukesh>
2025-12-25 22:18:38 -0800

    build succeed

--- llama-spm/Sources/llama/llama.swift
+++
@@ -1,9 +0,0 @@
-//
-//  llama.swift
-//  llama-spm
-//
-//  Created for OLMoE.swift
-//
-
-@_exported import llama
-@_exported import llama_mtmd
---
+++ llama-spm/Tests/llamaTests/llamaTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import llama
+
+final class llamaTests: XCTestCase {
+    func testModuleLoads() {
+        XCTAssertTrue(true)
+    }
+}
